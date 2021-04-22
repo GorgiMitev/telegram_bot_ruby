9f4bc43f96f918f70e906e999d270e62ffcd2400
Gorgi <gorgimitev@gmail.com>
2021-04-22 17:38:31 +0200

    making changes after failed test

--- lib/bot.rb
+++ lib/bot.rb
@@ -14,16 +14,20 @@ class Bot
 
         when '/stop'
 
-          bot.api.send_message(chat_id: message.chat.id, text: "Goodbye, #{message.from.first_name}. Have a nice day!", date: message_date)
+          bot.api.send_message(chat_id: message.chat.id, text: "Goodbye, #{message.from.first_name}. Have a nice day!", date: message.date)
+
         when '/motivation'
-            motivation_quote = Motivation.new.random_quote
-            bot.api.send_message(chat_id: message.chat.id, text: "#{motivation_quote['text']}", date: message.date)  
+
+          motivation_quote = Motivation.new.random_quote
+          bot.api.send_message(chat_id: message.chat.id, text: (motivation_quote['text']).to_s, date: message.date)
+
         when '/jokes'
-            joke_quote = Jokes.new.random_jokes
-            bot.api.send_message(chat_id: message.chat.id, text: "#{joke_quote['joke']}", date: message.date) 
+
+          joke_quote = Jokes.new.make_request
+          bot.api.send_message(chat_id: message.chat.id, text: (joke_quote['joke']).to_s, date: message.date)
         else
 
-            bot.api.send_message(chat_id: message.chat.id, text: "Invalid entry, #{message.from.first_name}, you need to use  /start,  /stop , /motivation or /jokes")
+          bot.api.send_message(chat_id: message.chat.id, text: "Invalid entry, #{message.from.first_name}, you need to use  /start,  /stop , /motivation or /jokes")
         end
       end
     end
