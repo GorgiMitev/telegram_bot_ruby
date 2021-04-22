eab5ff6d4638e00278204159953e6b81bfd9e48a
Gorgi <gorgimitev@gmail.com>
2021-04-22 14:32:11 +0200

    testing the interface between the telegram bot and our program with a random message

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,3 +1,9 @@
-require './lib/bot'
+require 'telegram/bot'
 
 token = "1767471390:AAExulZahTiJBR7q_lckVAcseWbbKRBeQfc"
+
+Telegram::Bot::Client.run(token) do |bot|
+    bot.listen do |message|
+        bot.api.send_message(chat_id: message.chat.id, text: "Hello, I am a bot!")
+    end 
+end
