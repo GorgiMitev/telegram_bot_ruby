b8f72e28933803a0ff820537d76b8488be389aab
Gorgi <gorgimitev@gmail.com>
2021-04-22 15:12:21 +0200

    Add different cases for when the chat starts and when it ends

--- lib/bot.rb
+++ lib/bot.rb
@@ -4,6 +4,14 @@ token = "1767471390:AAExulZahTiJBR7q_lckVAcseWbbKRBeQfc"
 
 Telegram::Bot::Client.run(token) do |bot|
     bot.listen do |message|
-        bot.api.send_message(chat_id: message.chat.id, text: "Hello, I am a bot!")
+        case message.text
+        when '/start'
+                
+        bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}.")
+        
+        when '/stop'
+
+        bot.api.send_message(chat_id: message.chat.id, text: "Goodbye, #{message.from.first_name}. Have a nice day!", date: message_date)
     end 
+end
 end
