be7da0dbce0ec15762792be0ff7beb4072a467da
Gorgi <gorgimitev@gmail.com>
2021-04-22 16:14:55 +0200

    adding the logic behind the random motivational quotes

--- lib/bot.rb
+++ lib/bot.rb
@@ -2,18 +2,21 @@ require 'telegram/bot'
 require_relative 'motivation.rb'
 require_relative 'jokes.rb'
 
-token = "1767471390:AAExulZahTiJBR7q_lckVAcseWbbKRBeQfc"
-
-Telegram::Bot::Client.run(token) do |bot|
-    bot.listen do |message|
+class Bot
+  def initialize
+    token = '1767471390:AAExulZahTiJBR7q_lckVAcseWbbKRBeQfc'
+    Telegram::Bot::Client.run(token) do |bot|
+      bot.listen do |message|
         case message.text
         when '/start'
-                
-        bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}.")
-        
+
+          bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}.")
+
         when '/stop'
 
-        bot.api.send_message(chat_id: message.chat.id, text: "Goodbye, #{message.from.first_name}. Have a nice day!", date: message_date)
-    end 
+          bot.api.send_message(chat_id: message.chat.id, text: "Goodbye, #{message.from.first_name}. Have a nice day!", date: message_date)
+      end
+      end
+    end
 end
 end
