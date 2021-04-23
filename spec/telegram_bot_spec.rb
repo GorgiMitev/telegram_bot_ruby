30faa0c7b3b0ef38a4a4426779a1539e6e3d463d
Gorgi <gorgimitev@gmail.com>
2021-04-23 11:58:46 +0200

    creating the tests for our Jokes clas

--- spec/telegram_bot_spec.rb
+++ spec/telegram_bot_spec.rb
@@ -2,25 +2,44 @@ require_relative '../lib/motivation.rb'
 require_relative '../lib/jokes.rb'
 
 RSpec.describe Motivation do
-    describe '#make_request' do
-      let(:request) { Motivation.new }
-      let(:request_json) { request.make_request }
-  
-      it 'should return json response when the request is sucessful' do
-        expect(request_json.class).to eql(Array)
-      end
-    end
+  describe '#make_request' do
+    let(:request) { Motivation.new }
+    let(:request_json) { request.make_request }
 
-    describe '#random_quote' do
-      let(:random) { Motivation.new }
-      let(:request_random) { random.random_quote }
-  
-      it 'should return an Hash' do
-        expect(request_random.class).to eql(Hash)
-      end
-  
-      it 'should return a key and a value' do
-        expect(request_random.length).to eql(2)
-      end
+    it 'should return a JSON object' do
+      expect(request_json.class).to eql(Array)
+    end
+    
+    it 'returns a non-empty JSON object' do
+      expect(request_json.length).not_to eql(0)
     end
   end
+
+  describe '#random_quote' do
+    let(:random) { Motivation.new }
+    let(:request_random) { random.random_quote }
+
+    it 'should return a Hash' do
+      expect(request_random.class).to eql(Hash)
+    end
+
+    it 'should return a key and a value' do
+      expect(request_random.length).to eql(2)
+    end
+  end
+end
+
+RSpec.describe Jokes do
+  describe '#make_request' do
+    let(:request) { Jokes.new }
+    let(:request_json) { request.make_request }
+
+    it 'when the request is sucessful should return a hash' do
+      expect(request_json.class).to eql(Hash)
+    end
+
+    it 'returns a non-empty JSON object' do
+      expect(request_json.length).not_to eql(0)
+    end
+  end
+end
