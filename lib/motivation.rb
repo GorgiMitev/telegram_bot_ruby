af0bcfa4a57308a573c0088dab50a34b5a150cc3
Gorgi Mitev <75483162+Solenka1@users.noreply.github.com>
2021-04-26 13:33:00 +0200

    Update motivation.rb

--- lib/motivation.rb
+++ lib/motivation.rb
@@ -5,7 +5,9 @@ require_relative 'bot'
 
 # Description/Explanation of Motivaton class which generates random motivation quotes
 class Motivation
-  @quote_values = @make_request
+  def initialize
+    @quote_values = make_request
+  end
 
   def random_quote
     @quote_values = @quote_values.sample
